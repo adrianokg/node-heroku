2b9ba32269b29bdbdf3e957d206dfffa07b09b46
Adriano <adrianokgroff@gmail.com>
2020-02-19 22:23:56 -0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(5000);
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+app.listen(PORT);
